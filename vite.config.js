bb12162b7cfe997f3baff5eedb962bf1404e47a0
ahmedebrahem0 <amezo2866@gmail.com>
2025-10-26 23:18:29 +0300

    build and deploy

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,13 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const isGhPages = env.GITHUB_PAGES === "true";
-  const repo = env.BASE_PATH || "FreshCart";
-
-  return {
-    base: isGhPages ? `/${repo}/` : "/",
-    plugins: [react()],
-    build: {
-      outDir: "dist",
-      assetsDir: "assets",
-      emptyOutDir: true,
-    },
-    server: {
-      port: 3000,
-      open: true,
-    },
-  };
+export default defineConfig({
+  base: "/FreshCart/", // اسم الريبو
+  plugins: [react()],
+  build: {
+    outDir: "docs", // اخلي البيلد يطلع في docs
+    emptyOutDir: true,
+    assetsDir: "assets",
+  },
+  server: { port: 3000 },
 });
